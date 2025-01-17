38cbcb277f92c10a13b8c1c67d6646acc8c9e0cf
Dev Kheradiya <devnkheradiya@gmail.com>
2025-01-17 13:17:18 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.bxslider-main').bxSlider({
+        mode: 'vertical',
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 360,
+        slideMargin: 10,
+        controls: false,
+        pager: true,
+        auto: true,
+        autoStart: true,
+        onSliderLoad: function () {
+            $(".bx-pager").css({
+                position: "absolute",
+                top: "50%",
+                right: "40px",
+                transform: "translateY(10%)",
+                display: "flex",
+                "flex-direction": "column",
+                gap: "10px",
+                "align-items": "end"
+            });
+            $(".bx-pager-link.active").css("background-color", "#ed017f");
+        },
+        onSlideAfter: function () {
+            $(".bx-pager-link").css("background-color", "#666");
+            $(".bx-pager-link.active").css("background-color", "#ed017f");
+        },
+    });
+    $('#bxslider').bxSlider({
+        mode: 'horizontal',
+        minSlides: 4,
+        maxSlides: 4,
+        slideWidth: 360,
+        slideMargin: 10,
+        controls: false,
+        pager: true,
+        auto: true,
+        autoStart: true,
+        onSlideAfter: function () {
+            $(".bx-pager-link").css('background-color', "#666");
+            $(".bx-pager-link.active").css('background-color', "#ed017f");
+        }
+    });
+    $(".bx-pager-link").css('background-color', "#666");
+    $(".bx-pager-link.active").css('background-color', "#ed017f");
+    $(".bx-wrapper").css('max-width', "none");
+})
